7cff2bf22c4e7c36936721238c52bdf9eb06c170
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-05 17:19:59 +0000

    Done.

---
+++ learning-cases.rb
@@ -0,0 +1,21 @@
+#greeting = "Hey you, nerd!"
+
+#case greeting
+#  when "Hey you, nerd!"
+#    puts "Ohno"
+#  when "Hi, how are you?"
+#    puts "Ohyes"
+#end
+
+weather_now = "?"
+
+case weather_now
+  when "sunny"
+    puts "Grab sunscreen!"
+  when "raining"
+    puts "Grab an umbrella!"
+  when "snowing"
+    puts "Bundle up!"
+  else
+    puts "Wildcard, baby. Good luck!"
+end
